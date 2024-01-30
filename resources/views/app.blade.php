66b78be2ac275aff70fba72ff44f5a3f04efd9c2
Yannick Müller <mail@ynnk.dev>
2024-01-30 08:12:05 +0100

    add basic website layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,74 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased text-white">
+    <header class="bg-transparent w-full h-16 absolute px-4 border-b z-10 border-gray-700 flex items-center justify-between">
+        <h1 class="inline-block font-bold text-3xl">M306</h1>
+    </header>
+    <main class="!h-[calc(100vh-3rem)] bg-[#050514] p-4 pt-20 backdrop-blur-md overflow-hidden">
+        <div>
+            @inertia
+        </div>
+        <div id="bg-1"></div>
+        <div id="bg-2"></div>
+    </main>
+    <footer class="bg-[#0d1117] w-full h-12  flex items-center justify-between px-4 border-t border-gray-700">
+        <span>&copy; bbzbl-it.dev 2024</span>
+        <span id="links">
+            <a href="mailto:support@bbzbl-it.dev">
+                <i class="fa-regular fa-envelope"></i>
+            </a>
+            <a href="https://github.com/bbzblit">
+                <i class="fa-brands fa-github cursor-pointer px-4"></i>
+            </a>
+            <a href="https://github.com/bbzblit/m306 px-4">
+                <i class="fa-solid fa-code"></i>
+            </a>
+        </span>
+    </footer>
+</body>
+
 </html>
+
+<style>
+    #bg-1 {
+        background-image: url("img/bg-1.png");
+        height: 610px;
+        width: 977px;
+        background-color: transparent;
+        rotate: -60deg;
+        scale: 2;
+        position: absolute;
+        top: 10%;
+        right: -10%;
+    }
+
+    #bg-2 {
+        background-image: url("img/bg-2.png");
+        height: 610px;
+        width: 977px;
+        background-color: transparent;
+        scale: 2;
+        position: absolute;
+        top: 20%;
+        rotate: 60deg;
+        z-index: 200;
+        background-size: 100% 150%;
+    }
+</style>
