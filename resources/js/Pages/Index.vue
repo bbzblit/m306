1d7a0aba63b97fcf11ddc2055716e0424babe784
Yannick Müller <mail@ynnk.dev>
2024-03-12 08:30:59 +0100

    code review

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,27 +1,31 @@
 <template>
     <div v-if="authentificated">
         <h1 class="text-3xl">Welcome back, {{ userName }}</h1>
-
     </div>
     <div v-else>
         <h1 class="text-3xl">Hello Stranger</h1>
     </div>
+
+    <SetPreview title="Test"></SetPreview>
 </template>
 
 <script>
-export default{
+import SetPreview from '../Components/SetPreview.vue';
+
+export default {
     name: "Index",
+    components: [SetPreview],
     props: {
         userName: {
             type: String,
-            default: null
-        }
+            default: null,
+        },
     },
 
     computed: {
-        authentificated(){
+        authentificated() {
             return this.userName !== null;
-        }
-    }
-}
+        },
+    },
+};
 </script>
